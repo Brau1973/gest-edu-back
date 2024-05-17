5eabdfbd6a9fe0f814dd85ff77a8d83e4011fc1a
brauliojaureguy <brauliojaureguy12@gmail.com>
2024-05-17 18:05:12 -0300

    fix dummy data asignaturas + dev profile

--- src/main/java/com/tecnoinf/gestedu/repositories/AsignaturaRepository.java
+++ src/main/java/com/tecnoinf/gestedu/repositories/AsignaturaRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AsignaturaRepository extends JpaRepository<Asignatura, Long>{
     Page<Asignatura> findAllByCarreraId(Long id, Pageable pageable);
     boolean existsByNombreAndCarreraId(String nombre, Long carreraId);
+    boolean existsByNombre(String comunicacionOralYEscrita);
 }
