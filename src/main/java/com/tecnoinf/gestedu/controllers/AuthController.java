20d1ea3fa81e1a699ea405662b0364f7dc38d21b
silBH <berrueta.hermida@gmail.com>
2024-05-21 18:51:00 -0300

    endpoint cierre secion

---
+++ src/main/java/com/tecnoinf/gestedu/controllers/AuthController.java
@@ -0,0 +1,33 @@
+package com.tecnoinf.gestedu.controllers;
+
+import com.tecnoinf.gestedu.dtos.usuario.AuthLoginRequest;
+import com.tecnoinf.gestedu.dtos.usuario.AuthResponse;
+import com.tecnoinf.gestedu.services.UserDetailsServiceImpl;
+import com.tecnoinf.gestedu.util.JwtUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping()
+@Tag(name = "Auth", description = "API para operaciones de Autenticación")
+public class AuthController {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Operation(summary = "Login de usuario")
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid AuthLoginRequest userRequest) {
+        return new ResponseEntity<>(this.userDetailsService.loginUser(userRequest), HttpStatus.OK);
+    }
+
+}
