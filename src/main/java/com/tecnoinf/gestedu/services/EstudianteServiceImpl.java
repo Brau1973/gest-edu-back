8730aa5072fc31d16c8d5ba300f540f686799df0
silBH <berrueta.hermida@gmail.com>
2024-05-25 15:53:19 -0300

    feature/buscarEstudiante ok

---
+++ src/main/java/com/tecnoinf/gestedu/services/EstudianteServiceImpl.java
@@ -0,0 +1,52 @@
+package com.tecnoinf.gestedu.services;
+
+import com.tecnoinf.gestedu.dtos.usuario.BasicInfoUsuarioDTO;
+import com.tecnoinf.gestedu.models.Estudiante;
+import com.tecnoinf.gestedu.models.Usuario;
+import com.tecnoinf.gestedu.repositories.UsuarioRepository;
+import jakarta.persistence.Basic;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EstudianteServiceImpl implements EstudianteService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public Page<BasicInfoUsuarioDTO> obtenerEstudiantes(Pageable pageable) {
+        List<BasicInfoUsuarioDTO> estudianteList = new ArrayList<>();
+        Page<Usuario> estudiantes = usuarioRepository.findAll(pageable);
+        if(estudiantes != null){
+            for(Usuario estudiante : estudiantes){
+                if(estudiante instanceof Estudiante){
+                    BasicInfoUsuarioDTO dto = new BasicInfoUsuarioDTO(estudiante);
+                    estudianteList.add(dto);
+                }
+            }
+        }
+        return new PageImpl<>(estudianteList, pageable, estudianteList.size());
+    }
+
+    @Override
+    public Optional<BasicInfoUsuarioDTO> obtenerEstudiantePorCi(String ci) {
+        Optional<Usuario> estudiante = usuarioRepository.findByCi(ci);
+        if(estudiante.isPresent() && estudiante.get() instanceof Estudiante){
+            return Optional.of(new BasicInfoUsuarioDTO(estudiante.get()));
+        }
+        return Optional.empty();
+    }
+}
