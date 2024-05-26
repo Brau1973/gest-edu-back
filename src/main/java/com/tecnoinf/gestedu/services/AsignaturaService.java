05c14bd7cbf9d4db0196ef38ac2fd898be5e5f32
Braulio Jaureguy <55444408+Brau1973@users.noreply.github.com>
2024-05-25 22:32:15 -0300

    get asignatura by id (#26)

--- src/main/java/com/tecnoinf/gestedu/services/AsignaturaService.java
+++ src/main/java/com/tecnoinf/gestedu/services/AsignaturaService.java
@@ -10,4 +10,5 @@ public interface AsignaturaService {
     List<AsignaturaDTO> getPrevias(Long asignaturaId);
     AsignaturaDTO addPrevia(Long asignaturaId, Long previaId);
     List<AsignaturaDTO> getNoPrevias(Long asignaturaId);
+    AsignaturaDTO getAsignaturaById(Long id);
 }
