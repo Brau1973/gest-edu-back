8f4be37de9f4f42cadd6eed9543b4e56f4dafcb1
silBH <berrueta.hermida@gmail.com>
2024-05-26 22:05:43 -0300

    listar examenes de una asignatura

--- src/main/java/com/tecnoinf/gestedu/repositories/ExamenRepository.java
+++ src/main/java/com/tecnoinf/gestedu/repositories/ExamenRepository.java
@@ -2,12 +2,15 @@ package com.tecnoinf.gestedu.repositories;
 
 import com.tecnoinf.gestedu.models.Asignatura;
 import com.tecnoinf.gestedu.models.Examen;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface ExamenRepository extends JpaRepository<Examen, Long> {
     boolean existsByFechaAndAsignatura(LocalDateTime fecha, Asignatura asignatura);
+    List<Examen> findByAsignaturaId(Long asignaturaId);
 }
