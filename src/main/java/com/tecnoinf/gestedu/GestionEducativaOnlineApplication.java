4401f45d45c5644a4ae9800354a637684094de4c
Gonzalo Cánepa <gcanepa28@gmail.com>
2024-05-28 03:14:54 -0300

    Registrar horario de curso de asignatura

--- src/main/java/com/tecnoinf/gestedu/GestionEducativaOnlineApplication.java
+++ src/main/java/com/tecnoinf/gestedu/GestionEducativaOnlineApplication.java
@@ -1,28 +1,27 @@
 package com.tecnoinf.gestedu;
 
+import com.tecnoinf.gestedu.controllers.CursoController;
+import com.tecnoinf.gestedu.dtos.curso.HorarioCursoDTO;
+import com.tecnoinf.gestedu.enumerados.DiaSemana;
+import com.tecnoinf.gestedu.enumerados.Estado;
 import com.tecnoinf.gestedu.models.*;
-import com.tecnoinf.gestedu.models.enums.*;
-import com.tecnoinf.gestedu.models.enums.Estado;
-import com.tecnoinf.gestedu.models.enums.EstadoInscripcionCarrera;
-import com.tecnoinf.gestedu.models.enums.EstadoTramite;
-import com.tecnoinf.gestedu.models.enums.TipoTramite;
-import com.tecnoinf.gestedu.repositories.*;
+import com.tecnoinf.gestedu.repositories.AsignaturaRepository;
+import com.tecnoinf.gestedu.repositories.CarreraRepository;
+import com.tecnoinf.gestedu.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 @SpringBootApplication
-@EnableAsync
 public class GestionEducativaOnlineApplication {
 
 	@Value("${spring.jpa.hibernate.ddl-auto}")
@@ -33,30 +32,23 @@ public class GestionEducativaOnlineApplication {
 	}
 
 	@Bean
-	public CommandLineRunner initData(UsuarioRepository usuarioRepository, CarreraRepository carreraRepository, AsignaturaRepository asignaturaRepository,
-									  InscripcionCarreraRepository inscripcionCarreraRepository, TramiteRepository tramiteRepository,
-									  DocenteRepository docenteRepository,  CursoRepository cursoRepository, PeriodoExamenRepository periodoExamenRepository ,
-									  ExamenRepository examenRepository, InscripcionCursoRepository inscripcionCursoRepository,
-									  InscripcionExamenRepository inscripcionExamenRepository) {
-
+	public CommandLineRunner initData(UsuarioRepository usuarioRepository, CarreraRepository carreraRepository, AsignaturaRepository asignaturaRepository) {
 		return (args) -> {
-			if(ddlAuto.equals("create") || ddlAuto.equals("create-drop")) {
-				Estudiante estudiante1 = new Estudiante();
-				Estudiante estudiante2 = new Estudiante();
-				Estudiante estudiante3 = new Estudiante();
+			if(ddlAuto.equals("update") || ddlAuto.equals("create-drop")) {
+
 
 				//-----USUARIOS-----
 				PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-				if (usuarioRepository.findByCi("61647956").isEmpty()) {
+				if (usuarioRepository.findByCi("67141245").isEmpty()) {
 					Usuario user = new Administrador();
-					user.setNombre("Pedro");
-					user.setApellido("Perez");
-					user.setEmail("PedroPeAdmin@mail.com");
+					user.setNombre("NombreAdminInitData");
+					user.setApellido("admin");
+					user.setEmail("adminInitData@yahoo.com");
 					user.setPassword(passwordEncoder.encode("1234"));
-					user.setCi("61647956");
-					user.setTelefono("091367159");
-					user.setDomicilio("Lindoro Forteza 5548");
+					user.setCi("67141245");
+					user.setTelefono("1234567");
+					user.setDomicilio("calle 123");
 					user.setIsEnable(true);
 					user.setAccountNonExpired(true);
 					user.setAccountNonLocked(true);
@@ -64,70 +56,45 @@ public class GestionEducativaOnlineApplication {
 					usuarioRepository.save(user);
 				}
 
-				if (usuarioRepository.findByCi("42569843").isEmpty()) {
-					estudiante1.setNombre("Luis");
-					estudiante1.setApellido("Diaz");
-					estudiante1.setEmail("LuisDiaz@mail.com");
-					estudiante1.setPassword(passwordEncoder.encode("1234"));
-					estudiante1.setCi("42569843");
-					estudiante1.setTelefono("093874635");
-					estudiante1.setDomicilio("Elias Regules 7898");
-					estudiante1.setIsEnable(true);
-					estudiante1.setAccountNonExpired(true);
-					estudiante1.setAccountNonLocked(true);
-					estudiante1.setCredentialsNonExpired(true);
-					usuarioRepository.save(estudiante1);
-
-					estudiante2.setNombre("Horacio");
-					estudiante2.setApellido("Hernandez");
-					estudiante2.setEmail("HoriHernandez@mail.com");
-					estudiante2.setPassword(passwordEncoder.encode("1234"));
-					estudiante2.setCi("61253594");
-					estudiante2.setTelefono("096558132");
-					estudiante2.setDomicilio("Pitagoras 1122");
-					estudiante2.setIsEnable(true);
-					estudiante2.setAccountNonExpired(true);
-					estudiante2.setAccountNonLocked(true);
-					estudiante2.setCredentialsNonExpired(true);
-					usuarioRepository.save(estudiante2);
-
-					estudiante3.setNombre("Ana Maria");
-					estudiante3.setApellido("Diaz");
-					estudiante3.setEmail("AnaMariaDiaz@mail.com");
-					estudiante3.setPassword(passwordEncoder.encode("1234"));
-					estudiante3.setCi("36984582");
-					estudiante3.setTelefono("095664889");
-					estudiante3.setDomicilio("Cmno Carrasco 2551");
-					estudiante3.setIsEnable(true);
-					estudiante3.setAccountNonExpired(true);
-					estudiante3.setAccountNonLocked(true);
-					estudiante3.setCredentialsNonExpired(true);
-					usuarioRepository.save(estudiante3);
+				if (usuarioRepository.findByCi("77141245").isEmpty()) {
+					Usuario estudiante = new Estudiante();
+					estudiante.setNombre("NombreEstudianteInitData");
+					estudiante.setApellido("estudiante");
+					estudiante.setEmail("estudianteInitData@yahoo.com");
+					estudiante.setPassword(passwordEncoder.encode("1234"));
+					estudiante.setCi("77141245");
+					estudiante.setTelefono("1234567");
+					estudiante.setDomicilio("calle 123");
+					estudiante.setIsEnable(true);
+					estudiante.setAccountNonExpired(true);
+					estudiante.setAccountNonLocked(true);
+					estudiante.setCredentialsNonExpired(true);
+					usuarioRepository.save(estudiante);
 				}
-				if (usuarioRepository.findByCi("42687516").isEmpty()) {
+				if (usuarioRepository.findByCi("87141245").isEmpty()) {
 					Usuario funcionario = new Funcionario();
-					funcionario.setNombre("Carla");
-					funcionario.setApellido("Miranda");
-					funcionario.setEmail("CarlaMiranda@mail.com");
+					funcionario.setNombre("NombreFuncionarioInitData");
+					funcionario.setApellido("funcionario");
+					funcionario.setEmail("funcionarioInitData@yahoo.com");
 					funcionario.setPassword(passwordEncoder.encode("1234"));
-					funcionario.setCi("42687516");
-					funcionario.setTelefono("097895632");
-					funcionario.setDomicilio("Bv Artigas 3556");
+					funcionario.setCi("87141245");
+					funcionario.setTelefono("1234567");
+					funcionario.setDomicilio("calle 123");
 					funcionario.setIsEnable(true);
 					funcionario.setAccountNonExpired(true);
 					funcionario.setAccountNonLocked(true);
 					funcionario.setCredentialsNonExpired(true);
 					usuarioRepository.save(funcionario);
 				}
-				if (usuarioRepository.findByCi("31247689").isEmpty()) {
+				if (usuarioRepository.findByCi("97141245").isEmpty()) {
 					Usuario coordinador = new Coordinador();
-					coordinador.setNombre("Juan Pablo");
-					coordinador.setApellido("Salinas");
-					coordinador.setEmail("JuanPSalinas@mail.com");
+					coordinador.setNombre("NombreCoordinadorInitData");
+					coordinador.setApellido("coordinador");
+					coordinador.setEmail("coordinadorInitData@yahoo.com");
 					coordinador.setPassword(passwordEncoder.encode("1234"));
-					coordinador.setCi("31247689");
-					coordinador.setTelefono("094658759");
-					coordinador.setDomicilio("Av Italia 6554");
+					coordinador.setCi("97141245");
+					coordinador.setTelefono("1234567");
+					coordinador.setDomicilio("calle 123");
 					coordinador.setIsEnable(true);
 					coordinador.setAccountNonExpired(true);
 					coordinador.setAccountNonLocked(true);
@@ -155,12 +122,6 @@ public class GestionEducativaOnlineApplication {
 					savedCarrera2 = carreraRepository.save(carrera2);
 				}
 
-				Carrera carrera3 = new Carrera();
-				carrera3.setNombre("Tecnologo Audiovisual InitData");
-				carrera3.setDescripcion("Carrera de tecnologo audiovisual donde se enseña a editar videos y sonido");
-				carrera3.setExistePlanEstudio(true);
-				Carrera savedCarrera3 = carreraRepository.save(carrera3);
-
 				//-----ASIGNATURAS-----
 				// SOLO VERIFICO SI EXISTE COE COMO PARA DARME CUENTA SI SE INSERTO LA DUMMY DATA PREVIAMENTE O NO (PARA SABER SI SE EJECUTO CON CREATE O UPDATE)
 				if (!asignaturaRepository.existsByNombre("Comunicacion oral y Escrita")) {
@@ -171,150 +132,14 @@ public class GestionEducativaOnlineApplication {
 					createAsignaturaInitData(asignaturaRepository, "Diseño de Interfaz", "Asignatura de diseño de interfaz de usuario", 4, 0, savedCarrera2);
 					createAsignaturaInitData(asignaturaRepository, "Experiencia de Usuario", "Asignatura de experiencia de usuario", 3,  0, savedCarrera2);
 					createAsignaturaInitData(asignaturaRepository, "Prototipado", "Asignatura de prototipado de interfaces", 2,  0, savedCarrera2);
-
-					createAsignaturaInitData(asignaturaRepository, "Grabacion con camara PRO", "Grabacion Full HD 4k", 2, 1, savedCarrera3);
-					createAsignaturaInitData(asignaturaRepository, "Adobe PRO", "Edicion de videos", 8, 2 , savedCarrera3);
-					createAsignaturaInitData(asignaturaRepository, "Photoshop", "Photoshop con adobe ilustrator", 1, 3 ,savedCarrera3);
 				}
-
-				// -----TRAMITES-----
-				createTramiteInitData(tramiteRepository, estudiante1, savedCarrera3, TipoTramite.INSCRIPCION_A_CARRERA, EstadoTramite.PENDIENTE);
-				createTramiteInitData(tramiteRepository, estudiante1, savedCarrera1, TipoTramite.INSCRIPCION_A_CARRERA, EstadoTramite.PENDIENTE);
-				createTramiteInitData(tramiteRepository, estudiante2, savedCarrera3, TipoTramite.INSCRIPCION_A_CARRERA, EstadoTramite.PENDIENTE);
-				createTramiteInitData(tramiteRepository, estudiante2, savedCarrera1, TipoTramite.INSCRIPCION_A_CARRERA, EstadoTramite.PENDIENTE);
-
-				createTramiteInitData(tramiteRepository, estudiante3, savedCarrera1, TipoTramite.INSCRIPCION_A_CARRERA, EstadoTramite.ACEPTADO);
-				createTramiteInitData(tramiteRepository, estudiante3, savedCarrera3, TipoTramite.INSCRIPCION_A_CARRERA, EstadoTramite.ACEPTADO);
-				createTramiteInitData(tramiteRepository, estudiante3, savedCarrera1, TipoTramite.SOLICITUD_DE_TITULO, EstadoTramite.PENDIENTE);
-				createTramiteInitData(tramiteRepository, estudiante3, savedCarrera3, TipoTramite.SOLICITUD_DE_TITULO, EstadoTramite.PENDIENTE);
-
-				// -----INSCRIPCIONCARRERA-----
-				InscripcionCarrera inscripcionCarrera1 = new InscripcionCarrera();
-				inscripcionCarrera1.setEstudiante(estudiante3);
-				inscripcionCarrera1.setCarrera(savedCarrera1);
-				inscripcionCarrera1.setEstado(EstadoInscripcionCarrera.CURSANDO);
-				inscripcionCarrera1.setFechaInscripcion(LocalDate.now());
-				inscripcionCarreraRepository.save(inscripcionCarrera1);
-
-				InscripcionCarrera inscripcionCarrera2= new InscripcionCarrera();
-				inscripcionCarrera2.setEstudiante(estudiante3);
-				inscripcionCarrera2.setCarrera(savedCarrera3);
-				inscripcionCarrera2.setEstado(EstadoInscripcionCarrera.COMPLETADA);
-				inscripcionCarrera2.setFechaInscripcion(LocalDate.now());
-				inscripcionCarreraRepository.save(inscripcionCarrera2);
-
-				//---- DOCENTES ----
-				Docente docente1 = new Docente();
-				docente1.setNombre("Fernando");
-				docente1.setApellido("Garcia");
-				docente1.setDocumento("42569897");
-				docenteRepository.save(docente1);
-
-				Docente docente2 = new Docente();
-				docente2.setNombre("Manuel");
-				docente2.setApellido("Gayoso");
-				docente2.setDocumento("42585843");
-				docenteRepository.save(docente2);
-
-				Docente docente3 = new Docente();
-				docente3.setNombre("Veronica");
-				docente3.setApellido("Gonzalez");
-				docente3.setDocumento("42569734");
-				docenteRepository.save(docente3);
-
-				//----- CURSOS -----
-				Curso curso1 = new Curso();
-				curso1.setEstado(Estado.ACTIVO);
-				curso1.setAsignatura(asignaturaRepository.findById(1L).get());
-				LocalDate fechaInicio = LocalDate.of(2025, 3, 15); // Año, Mes (1-12), Día
-				curso1.setFechaInicio(fechaInicio);
-				LocalDate fechaFin = LocalDate.of(2025, 07, 15); // Año, Mes (1-12), Día
-				curso1.setFechaFin(fechaFin);
-				curso1.setDocente(docente1);
-				curso1.setDiasPrevInsc(30);
-				cursoRepository.save(curso1);
-
-				Curso curso2 = new Curso();
-				curso2.setEstado(Estado.FINALIZADO);
-				curso2.setAsignatura(asignaturaRepository.findById(1L).get());
-				fechaInicio = LocalDate.of(2023, 3, 15); // Año, Mes (1-12), Día
-				curso1.setFechaInicio(fechaInicio);
-				fechaFin = LocalDate.of(2023, 07, 15); // Año, Mes (1-12), Día
-				curso2.setFechaFin(fechaFin);
-				curso2.setDocente(docente1);
-				curso2.setDiasPrevInsc(30);
-				cursoRepository.save(curso2);
-
-				//----- INSCRIPCIONCURSO -----
-				InscripcionCurso inscripcionCurso1 = new InscripcionCurso();
-				inscripcionCurso1.setEstudiante(estudiante2);
-				inscripcionCurso1.setCurso(curso1);
-				inscripcionCurso1.setCalificacion(CalificacionCurso.AEXAMEN);
-				inscripcionCursoRepository.save(inscripcionCurso1);
-
-				InscripcionCurso inscripcionCurso2 = new InscripcionCurso();
-				inscripcionCurso2.setEstudiante(estudiante3);
-				inscripcionCurso2.setCurso(curso1);
-				inscripcionCurso2.setCalificacion(CalificacionCurso.AEXAMEN);
-				inscripcionCursoRepository.save(inscripcionCurso2);
-
-				//----- PERIODO EXAMEN -----
-				PeriodoExamen periodoExamen1 = new PeriodoExamen();
-				periodoExamen1.setFechaInicio(LocalDateTime.now().plusDays(5));
-				periodoExamen1.setFechaFin(LocalDateTime.now().plusDays(10));
-				periodoExamen1.setCarrera(carreraRepository.findById(1L).get());
-				periodoExamenRepository.save(periodoExamen1);
-
-				//----- EXAMEN -----
-				Examen examen1 = new Examen();
-				examen1.setFecha(LocalDateTime.now().plusDays(7));
-				examen1.setDiasPrevInsc(20);
-				examen1.setEstado(Estado.ACTIVO);
-				examen1.setAsignatura(asignaturaRepository.findById(1L).get());
-				examen1.setDocentes(new ArrayList<>());
-				examen1.getDocentes().add(docenteRepository.findById(1L).get());
-				examenRepository.save(examen1);
-
-				Examen examen2 = new Examen();
-				examen2.setFecha(LocalDateTime.now().minusDays(30));
-				examen2.setDiasPrevInsc(20);
-				examen2.setEstado(Estado.FINALIZADO);
-				examen2.setAsignatura(asignaturaRepository.findById(1L).get());
-				examen2.setDocentes(new ArrayList<>());
-				examen2.getDocentes().add(docenteRepository.findById(1L).get());
-				examenRepository.save(examen2);
-
-				//----- INSCRIPCIONEXAMEN -----
-				InscripcionExamen inscripcionExamen1 = new InscripcionExamen();
-				inscripcionExamen1.setEstudiante(estudiante1);
-				inscripcionExamen1.setExamen(examen1);
-				inscripcionExamen1.setCalificacion(CalificacionExamen.PENDIENTE);
-				inscripcionExamenRepository.save(inscripcionExamen1);
-
-				InscripcionExamen inscripcionExamen2 = new InscripcionExamen();
-				inscripcionExamen2.setEstudiante(estudiante2);
-				inscripcionExamen2.setExamen(examen2);
-				inscripcionExamen2.setCalificacion(CalificacionExamen.PENDIENTE);
-				inscripcionExamenRepository.save(inscripcionExamen2);
-
 			}
 		};
 	}
 
-	private void createTramiteInitData(TramiteRepository tramiteRepository, Estudiante estudiante, Carrera savedCarrera, TipoTramite tipoTramite, EstadoTramite estadoTramite) {
-		Tramite tramite = new Tramite();
-		tramite.setUsuarioSolicitante(estudiante);
-		tramite.setTipo(tipoTramite);
-		tramite.setEstado(estadoTramite);
-		tramite.setFechaCreacion(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES));
-		tramite.setCarrera(savedCarrera);
-		tramiteRepository.save(tramite);
-	}
-
 	private void createAsignaturaInitData(AsignaturaRepository asignaturaRepository, String nombre, String descripcion, Integer creditos, Integer semestrePlanEstudio ,Carrera carrera) {
 		if(!asignaturaRepository.existsByNombreAndCarreraId(nombre, carrera.getId())){
-
-			Asignatura asignatura = new Asignatura(null, nombre, descripcion, creditos, semestrePlanEstudio, carrera, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+			Asignatura asignatura = new Asignatura(null, nombre, descripcion, creditos, semestrePlanEstudio, carrera);
 			asignaturaRepository.save(asignatura);
 		}
 	}
--- src/main/java/com/tecnoinf/gestedu/services/CursoServiceImpl.java
+++ src/main/java/com/tecnoinf/gestedu/services/CursoServiceImpl.java
@@ -1,11 +1,7 @@
 package com.tecnoinf.gestedu.services;
 
 import com.tecnoinf.gestedu.dtos.curso.CursoDTO;
-import com.tecnoinf.gestedu.dtos.curso.HorarioCursoDTO;
 import com.tecnoinf.gestedu.dtos.curso.HorarioDTO;
-import com.tecnoinf.gestedu.dtos.curso.InscripcionACursoDTO;
-import com.tecnoinf.gestedu.enumerados.CalificacionCursada;
-import com.tecnoinf.gestedu.enumerados.EstadoInscACurso;
 import com.tecnoinf.gestedu.exceptions.ResourceNotFoundException;
 import com.tecnoinf.gestedu.models.*;
 import com.tecnoinf.gestedu.repositories.*;
@@ -13,9 +9,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.*;
 
 @Service
 public class CursoServiceImpl implements CursoService{
@@ -24,141 +17,51 @@ public class CursoServiceImpl implements CursoService{
     private final DocenteRepository docenteRepository;
     private final EstudianteRepository estudianteRepository;
     private final InscripcionACursoRepository inscripcionACursoRepository;
+    private final HorarioRepository horarioRepository;
     private final ModelMapper modelMapper;
 
 
 
     @Autowired
-    public CursoServiceImpl(CursoRepository cursoRepository, AsignaturaRepository asignaturaRepository, DocenteRepository docenteRepository, EstudianteRepository estudianteRepository, InscripcionACursoRepository inscripcionACursoRepository, ModelMapper modelMapper) {
+    public CursoServiceImpl(CursoRepository cursoRepository, AsignaturaRepository asignaturaRepository, DocenteRepository docenteRepository, EstudianteRepository estudianteRepository, InscripcionACursoRepository inscripcionACursoRepository, HorarioRepository horarioRepository, ModelMapper modelMapper) {
         this.cursoRepository = cursoRepository;
         this.asignaturaRepository = asignaturaRepository;
         this.docenteRepository = docenteRepository;
         this.estudianteRepository = estudianteRepository;
         this.inscripcionACursoRepository = inscripcionACursoRepository;
+        this.horarioRepository = horarioRepository;
         this.modelMapper = modelMapper;
     }
 
     @Override
-    public CursoDTO createCurso(HorarioCursoDTO horarioCursoDTO){//CursoDTO cursoDTO, HorarioDTO horarioDTO, Long docenteId) {
-        Asignatura asignatura = asignaturaRepository.findById(horarioCursoDTO.getAsignaturaId())
-                .orElseThrow(() -> new ResourceNotFoundException("Asignatura not found with id " + horarioCursoDTO.getAsignaturaId()));
+    public CursoDTO createCurso(CursoDTO cursoDTO, HorarioDTO horarioDTO, Long docenteId) {
+        Asignatura asignatura = asignaturaRepository.findById(cursoDTO.getAsignaturaId())
+                .orElseThrow(() -> new ResourceNotFoundException("Asignatura not found with id " + cursoDTO.getAsignaturaId()));
 
-        if (horarioCursoDTO.getFechaInicio().compareTo(horarioCursoDTO.getFechaFin()) < 0) {
-            Curso curso = new Curso();
-            curso = modelMapper.map(horarioCursoDTO, Curso.class);
+        if (cursoDTO.getFechaInicio().compareTo(cursoDTO.getFechaFin()) < 0) {
+            Curso curso = modelMapper.map(cursoDTO, Curso.class);
             curso.setAsignaturas(asignatura);
             curso.setId(null);
 
-            HorarioDTO h = new HorarioDTO();
-            h.setId(horarioCursoDTO.getIdHorario());
-            h.setDia(horarioCursoDTO.getDia());
-            h.setHoraInicio(horarioCursoDTO.getHoraInicio());
-            h.setHoraFin(horarioCursoDTO.getHoraFin());
-            h.addCursosId(horarioCursoDTO.getIdCurso());
-            if (h.getHoraInicio().compareTo(h.getHoraFin()) < 0) {
-                Horario horario = modelMapper.map(h, Horario.class);
+            if (horarioDTO.getHoraInicio().compareTo(horarioDTO.getHoraFin()) < 0) {
+                Horario horario = modelMapper.map(horarioDTO, Horario.class);
                 curso.addHorario(horario);
+                boolean existeDocente = docenteRepository.existsById(docenteId);
+                if(existeDocente){
+                    Docente docente = docenteRepository.getById(docenteId);
+                    curso.addDocente(docente);
+                    Horario createdHorario = horarioRepository.save(horario);
+                    modelMapper.map(createdHorario, HorarioDTO.class);
+                    Curso createdCurso = cursoRepository.save(curso);
+                    return modelMapper.map(createdCurso, CursoDTO.class);
+                } else {
+                    throw new IllegalArgumentException("El docente no existe");
+                }
             } else {
                 throw new IllegalArgumentException("Hora de inicio debe ser anterior a la hora de fin");
             }
-
-            boolean existeDocente = docenteRepository.existsById(horarioCursoDTO.getIdDocente());
-
-            if(existeDocente){
-                Docente docente = docenteRepository.getById(horarioCursoDTO.getIdDocente());
-                curso.addDocente(docente);
-
-                Curso createdCurso = cursoRepository.save(curso);
-                return modelMapper.map(createdCurso, CursoDTO.class);
-            }
-                else{
-                throw new IllegalArgumentException("El docente no existe");
-            }
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("La fecha inicial debe ser anterior a la fecha final");
-        }
-    }
-
-    @Override
-    public boolean isEstudianteInscritoEnCurso(Long estudianteId, Long cursoId) {
-        Estudiante estudiante = new Estudiante();
-        estudiante.setId(estudianteId);
-
-        Curso curso = new Curso();
-        curso.setId(cursoId);
-
-        return inscripcionACursoRepository.existsByEstudianteAndCurso(estudiante, curso);
-    }
-
-    @Override
-    public List<InscripcionACursoDTO> listarInscripcionesPorEstudiante(Long estudianteId) {
-        List<InscripcionACurso> inscripciones = inscripcionACursoRepository.findByEstudianteId(estudianteId);
-        List<InscripcionACursoDTO> inscripcionACursoDTOList = null;
-        for(InscripcionACurso ins: inscripciones){
-            InscripcionACursoDTO auxInscripcion = new InscripcionACursoDTO();
-            auxInscripcion.setId(ins.getId());
-            auxInscripcion.setEstadoInscACurso(ins.getEstadoInscACurso());
-            auxInscripcion.setCalificacion(ins.getCalificacion());
-            auxInscripcion.setFechaInscripcion(ins.getFechaInscripcion());
-            auxInscripcion.setIdEstudiante(ins.getEstudiante().getId());
-            auxInscripcion.setIdCurso(ins.getCurso().getId());
-            inscripcionACursoDTOList.add(auxInscripcion);
-        }
-        return inscripcionACursoDTOList;
-    }
-
-    @Override
-    public InscripcionACursoDTO inscribirCurso(InscripcionACursoDTO inscripcionCurso){
-        Optional<Usuario> optionalEstudiante = estudianteRepository.findById(inscripcionCurso.getIdEstudiante());
-        Estudiante estudiante = (Estudiante) optionalEstudiante.orElseThrow(() -> new NoSuchElementException("Estudiante no encontrado"));
-
-        List<InscripcionACursoDTO> inscripcionACursoDTOList = listarInscripcionesPorEstudiante(inscripcionCurso.getIdEstudiante());
-        if(inscripcionACursoDTOList.isEmpty()){
-            //TODO fijarse un sistema para las previas
-            InscripcionACurso insc = new InscripcionACurso();
-            insc = modelMapper.map(inscripcionCurso, InscripcionACurso.class);
-            insc.setId(null);
-            insc.setCalificacion(CalificacionCursada.PENDIENTE);
-            insc.setEstudiante(estudiante);
-
-            insc.setEstadoInscACurso(EstadoInscACurso.CURSANDO);
-            LocalDate localDate = LocalDate.now();
-            Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-            insc.setFechaInscripcion(date);
-
-
-            insc.setCurso(cursoRepository.getReferenceById(inscripcionCurso.getIdCurso()));
-            InscripcionACurso createdInscripcion = inscripcionACursoRepository.save(insc);
-            return modelMapper.map(createdInscripcion, InscripcionACursoDTO.class);
-        }
-        else{
-            for(InscripcionACursoDTO auxInsc: inscripcionACursoDTOList){
-                if(auxInsc.equals(inscripcionCurso.getIdCurso())){
-                    if(auxInsc.getCalificacion().equals(CalificacionCursada.EXONERADO)){
-                        throw new IllegalArgumentException("No puedes inscribirte a un curso exonerado.");
-                    }
-                    else{
-                        //TODO fijarse un sistema para las previas
-                        InscripcionACurso insc = new InscripcionACurso();
-                        insc = modelMapper.map(inscripcionCurso, InscripcionACurso.class);
-                        insc.setId(null);
-                        insc.setCalificacion(CalificacionCursada.PENDIENTE);
-                        insc.setEstudiante(estudiante);
-
-                        insc.setEstadoInscACurso(EstadoInscACurso.CURSANDO);
-                        LocalDate localDate = LocalDate.now();
-                        Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-                        insc.setFechaInscripcion(date);
-
-
-                        insc.setCurso(cursoRepository.getReferenceById(inscripcionCurso.getIdCurso()));
-                        InscripcionACurso createdInscripcion = inscripcionACursoRepository.save(insc);
-                        return modelMapper.map(createdInscripcion, InscripcionACursoDTO.class);
-                    }
-                }
-            }
-            throw new IllegalArgumentException("Error al inscribirse al curso");
         }
     }
 }
