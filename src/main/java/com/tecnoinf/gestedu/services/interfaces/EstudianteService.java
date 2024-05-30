6c228c351159e5e9857fe3b9fabca021745759df
silBH <berrueta.hermida@gmail.com>
2024-05-30 20:24:43 -0300

    fix/carreraEstudianteInscripto endpoint nuevo

--- src/main/java/com/tecnoinf/gestedu/services/interfaces/EstudianteService.java
+++ src/main/java/com/tecnoinf/gestedu/services/interfaces/EstudianteService.java
@@ -1,5 +1,6 @@
 package com.tecnoinf.gestedu.services.interfaces;
 
+import com.tecnoinf.gestedu.dtos.asignatura.AsignaturaDTO;
 import com.tecnoinf.gestedu.dtos.carrera.BasicInfoCarreraDTO;
 import com.tecnoinf.gestedu.models.Carrera;
 import com.tecnoinf.gestedu.dtos.usuario.BasicInfoUsuarioDTO;
@@ -13,4 +14,5 @@ public interface EstudianteService {
     Page<BasicInfoUsuarioDTO> obtenerEstudiantes(Pageable pageable);
     Optional<BasicInfoUsuarioDTO> obtenerEstudiantePorCi(String ci);
     Page<BasicInfoCarreraDTO> getCarrerasInscripto(String email, Pageable pageable);
+    Page<AsignaturaDTO> obtenerAsignaturasAExamen(Long carreraId, String email, Pageable pageable);
 }
