8931c25b77eb135c1ad1c643fd214c81f3f8ec4b
silBH <78172947+silBH@users.noreply.github.com>
2024-05-31 18:02:27 -0300

    Feature/registrar calificaciones examen (#49)

--- src/main/java/com/tecnoinf/gestedu/controllers/ExamenController.java
+++ src/main/java/com/tecnoinf/gestedu/controllers/ExamenController.java
@@ -2,12 +2,15 @@ package com.tecnoinf.gestedu.controllers;
 
 import com.tecnoinf.gestedu.dtos.examen.CreateExamenDTO;
 import com.tecnoinf.gestedu.dtos.examen.ExamenDTO;
-import com.tecnoinf.gestedu.dtos.inscripcionCarrera.InscripcionCarreraDTO;
+import com.tecnoinf.gestedu.dtos.inscripcionExamen.InscripcionExamenCalificacionDTO;
 import com.tecnoinf.gestedu.dtos.inscripcionExamen.CreateInscripcionExamenDTO;
 import com.tecnoinf.gestedu.dtos.inscripcionExamen.InscripcionExamenDTO;
 import com.tecnoinf.gestedu.services.interfaces.ExamenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +25,7 @@ public class ExamenController {
 
     private final ExamenService examenService;
 
+    @Autowired
     public ExamenController(ExamenService examenService) {
         this.examenService = examenService;
     }
@@ -47,10 +51,33 @@ public class ExamenController {
         return ResponseEntity.ok().body(createdExamen);
     }
 
+    @Operation(summary = "Listar examenes pendientes de calificar")
+    @GetMapping("/examenes-pendientes")
+    //@PreAuthorize("hasAuthority('ROL_FUNCIONARIO')")
+    public ResponseEntity<Page<ExamenDTO>> listarExamenesPendientes(Pageable pageable) {
+        return new ResponseEntity<>(examenService.listarExamenesPendientes(pageable), HttpStatus.OK);
+    }
+
     @Operation(summary = "Listar estudiantes inscriptos a un examen")
     @GetMapping("/{id}/estudiantes-inscriptos")
     public ResponseEntity<List<InscripcionExamenDTO>> getEstudiantesInscriptos(@PathVariable Long id) {
         List<InscripcionExamenDTO> inscripciones = examenService.listarInscriptosExamen(id);
         return ResponseEntity.ok().body(inscripciones);
+    }
+
+    @Operation(summary = "Obtener calificaciones de examen")
+    @GetMapping("/{id}/calificar")
+    //@PreAuthorize("hasAuthority('ROL_FUNCIONARIO')")
+    public ResponseEntity<List<InscripcionExamenCalificacionDTO>> getCalificacionesExamen(@PathVariable Long id) {
+        List<InscripcionExamenCalificacionDTO> calificacionesExamen = examenService.obtenerCalificaciones(id);
+        return ResponseEntity.ok().body(calificacionesExamen);
+    }
+
+    @Operation(summary = "Registrar calificaciones de examen")
+    @PutMapping("/{id}/calificar")
+    //@PreAuthorize("hasAuthority('ROL_FUNCIONARIO')")
+    public ResponseEntity<List<InscripcionExamenCalificacionDTO>> registrarCalificaciones(@PathVariable Long id, @RequestBody List<InscripcionExamenCalificacionDTO> calificaciones) {
+        List<InscripcionExamenCalificacionDTO> calificacionesExamen = examenService.registrarCalificaciones(id, calificaciones);
+        return ResponseEntity.ok().body(calificacionesExamen);
     }
 }
--- src/main/java/com/tecnoinf/gestedu/services/interfaces/ExamenService.java
+++ src/main/java/com/tecnoinf/gestedu/services/interfaces/ExamenService.java
@@ -2,8 +2,11 @@ package com.tecnoinf.gestedu.services.interfaces;
 
 import com.tecnoinf.gestedu.dtos.examen.CreateExamenDTO;
 import com.tecnoinf.gestedu.dtos.examen.ExamenDTO;
+import com.tecnoinf.gestedu.dtos.inscripcionExamen.InscripcionExamenCalificacionDTO;
 import com.tecnoinf.gestedu.dtos.inscripcionExamen.CreateInscripcionExamenDTO;
 import com.tecnoinf.gestedu.dtos.inscripcionExamen.InscripcionExamenDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -11,4 +14,8 @@ public interface ExamenService {
     ExamenDTO altaExamen(CreateExamenDTO createExamenDto);
     InscripcionExamenDTO inscribirseExamen(CreateInscripcionExamenDTO inscripcionExamenDto);
     List<InscripcionExamenDTO> listarInscriptosExamen(Long id);
+    Page<ExamenDTO> listarExamenesPendientes(Pageable pageable);
+    List<InscripcionExamenCalificacionDTO> obtenerCalificaciones(Long id);
+    List<InscripcionExamenCalificacionDTO> registrarCalificaciones(Long id, List<InscripcionExamenCalificacionDTO> calificaciones);
+
 }
