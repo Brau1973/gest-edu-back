19a40b503c749d70753ab302d0ee319fda6c6661
Gonzalo Cánepa <gcanepa28@gmail.com>
2024-05-31 19:25:36 -0300

    Inscripcion Cursos

--- src/main/java/com/tecnoinf/gestedu/controllers/InscripcionCursoController.java
+++ src/main/java/com/tecnoinf/gestedu/controllers/InscripcionCursoController.java
@@ -12,6 +12,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/inscripcionCurso")
