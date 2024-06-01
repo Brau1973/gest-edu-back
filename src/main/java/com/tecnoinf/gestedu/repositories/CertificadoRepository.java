37515932a14a8287f26696905ba38c6f645c90ae
silBH <berrueta.hermida@gmail.com>
2024-06-01 19:05:57 -0300

    validar certificado

--- src/main/java/com/tecnoinf/gestedu/repositories/CertificadoRepository.java
+++ src/main/java/com/tecnoinf/gestedu/repositories/CertificadoRepository.java
@@ -3,5 +3,8 @@ package com.tecnoinf.gestedu.repositories;
 import com.tecnoinf.gestedu.models.Certificado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CertificadoRepository extends JpaRepository<Certificado, Long> {
+    Optional<Certificado> findByCodigoValidacion(String codigoValidacion);
 }
