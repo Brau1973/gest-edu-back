b31fd1cd954366cbcf09a9bb268861a9bd4769a4
silBH <berrueta.hermida@gmail.com>
2024-06-01 18:58:51 -0300

    andando certificado

--- src/main/java/com/tecnoinf/gestedu/repositories/InscripcionCarreraRepository.java
+++ src/main/java/com/tecnoinf/gestedu/repositories/InscripcionCarreraRepository.java
@@ -4,4 +4,5 @@ import com.tecnoinf.gestedu.models.InscripcionCarrera;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InscripcionCarreraRepository extends JpaRepository<InscripcionCarrera, Long> {
+    InscripcionCarrera findByEstudianteIdAndCarreraId(Long id, Long carreraId);
 }
