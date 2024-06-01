dfaff00503642a2c3b6cada87bf8ea49c97f2443
Gonzalo Cánepa <gcanepa28@gmail.com>
2024-06-01 16:00:53 -0300

    Registrar Calificaciones

--- src/main/java/com/tecnoinf/gestedu/controllers/InscripcionCursoController.java
+++ src/main/java/com/tecnoinf/gestedu/controllers/InscripcionCursoController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/inscripcionCurso")
