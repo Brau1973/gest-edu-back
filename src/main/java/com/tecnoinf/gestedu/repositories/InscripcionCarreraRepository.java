156550735151a0b4824cb349421d585ab5dd7c48
Gonzalo Cánepa <gcanepa28@gmail.com>
2024-06-02 01:50:02 -0300

    Squashed commit of the following:
    commit 9fedc7ef0b7ac7d54a97715d9bd71b0f259317ec
    Author: silBH <78172947+silBH@users.noreply.github.com>
    Date:   Sat Jun 1 21:15:58 2024 -0300
    
        Feature/solicitar certificado + desactivarCuentaUsuario (#57)
    
        * feature/desactivarCuentaUsuario

--- src/main/java/com/tecnoinf/gestedu/repositories/InscripcionCarreraRepository.java
+++ src/main/java/com/tecnoinf/gestedu/repositories/InscripcionCarreraRepository.java
@@ -9,5 +9,6 @@ import java.util.List;
 public interface InscripcionCarreraRepository extends JpaRepository<InscripcionCarrera, Long> {
     @Query("SELECT ic FROM InscripcionCarrera ic JOIN ic.estudiante e WHERE ic.estudiante.id = :estudianteId")
     List<InscripcionCarrera> findInscripcionCarreraEstudianteById(Long estudianteId);
+
     InscripcionCarrera findByEstudianteIdAndCarreraId(Long id, Long carreraId);
 }
