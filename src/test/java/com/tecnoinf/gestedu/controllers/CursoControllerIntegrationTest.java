83e04dd99862fa17452ced650716a888c7b270a0
Gonzalo Cánepa <christian.gonzalo.canepa@fing.edu.uy>
2024-06-03 15:29:28 -0300

    Obtener Estudiantes por Id de Curso. (#68)

--- src/test/java/com/tecnoinf/gestedu/controllers/CursoControllerIntegrationTest.java
+++ src/test/java/com/tecnoinf/gestedu/controllers/CursoControllerIntegrationTest.java
@@ -3,17 +3,15 @@ package com.tecnoinf.gestedu.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tecnoinf.gestedu.dtos.curso.CursoDTO;
 import com.tecnoinf.gestedu.dtos.curso.HorarioDTO;
-import com.tecnoinf.gestedu.models.Asignatura;
-import com.tecnoinf.gestedu.models.Carrera;
-import com.tecnoinf.gestedu.models.Curso;
-import com.tecnoinf.gestedu.models.Docente;
+import com.tecnoinf.gestedu.dtos.usuario.UsuarioDTO;
+import com.tecnoinf.gestedu.models.*;
+import com.tecnoinf.gestedu.models.enums.CalificacionCurso;
 import com.tecnoinf.gestedu.models.enums.DiaSemana;
 import com.tecnoinf.gestedu.models.enums.Estado;
-import com.tecnoinf.gestedu.repositories.AsignaturaRepository;
-import com.tecnoinf.gestedu.repositories.CarreraRepository;
-import com.tecnoinf.gestedu.repositories.CursoRepository;
-import com.tecnoinf.gestedu.repositories.DocenteRepository;
+import com.tecnoinf.gestedu.models.enums.EstadoInscripcionCarrera;
+import com.tecnoinf.gestedu.repositories.*;
 import com.tecnoinf.gestedu.services.interfaces.CursoService;
+import jakarta.validation.constraints.Email;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,7 +20,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -51,6 +56,15 @@ public class CursoControllerIntegrationTest {
 
     @Autowired
     private CarreraRepository carreraRepository;
+
+    @Autowired
+    private InscripcionCursoRepository inscripcionCursoRepository;
+
+    @Autowired
+    private InscripcionCarreraRepository inscripcionCarreraRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @Test
     @Transactional
@@ -132,5 +146,105 @@ public class CursoControllerIntegrationTest {
                 .andExpect(jsonPath("$.dia").value(DiaSemana.LUNES.name()))
                 .andExpect(jsonPath("$.horaInicio").value("09:00:00"))
                 .andExpect(jsonPath("$.horaFin").value("11:00:00"));
+    }
+
+    @Test
+    @Transactional
+    public void getEstudiantesByCurso() throws Exception {
+        //Crear Estudiante
+        Estudiante estudiante = new Estudiante();
+        estudiante.setCi("12345678");
+        estudiante.setNombre("John");
+        estudiante.setApellido("Doe");
+        estudiante.setEmail("johndoe@gmail.com");
+        estudiante.setPassword("12345678");
+        estudiante = usuarioRepository.save(estudiante);
+
+        //Crear Estudiante
+        Estudiante estudiante2 = new Estudiante();
+        estudiante2.setCi("12345679");
+        estudiante2.setNombre("Jane");
+        estudiante2.setApellido("Doe");
+        estudiante2.setEmail("janedoe28@gmail.com");
+        estudiante2.setPassword("12345678");
+        estudiante2 = usuarioRepository.save(estudiante2);
+
+        // Crear carrera
+        Carrera carrera = new Carrera();
+        carrera.setNombre("Carrera Test");
+        carrera = carreraRepository.save(carrera);
+
+        //InscripcionCarrera
+        InscripcionCarrera inscCarrera = new InscripcionCarrera();
+        inscCarrera.setCarrera(carrera);
+        inscCarrera.setEstado(EstadoInscripcionCarrera.CURSANDO);
+        inscCarrera.setFechaInscripcion(LocalDateTime.of(2025, 7, 15,0,0,0));
+        inscCarrera.setCreditosObtenidos(15);
+        inscCarrera.setEstudiante(estudiante);
+        inscCarrera = inscripcionCarreraRepository.save(inscCarrera);
+
+        InscripcionCarrera inscCarrera2 = new InscripcionCarrera();
+        inscCarrera2.setCarrera(carrera);
+        inscCarrera2.setEstado(EstadoInscripcionCarrera.CURSANDO);
+        inscCarrera2.setFechaInscripcion(LocalDateTime.of(2025, 7, 15,0,0,0));
+        inscCarrera2.setCreditosObtenidos(15);
+        inscCarrera2.setEstudiante(estudiante2);
+        inscCarrera2 = inscripcionCarreraRepository.save(inscCarrera2);
+        List<InscripcionCarrera> insCarreras = new ArrayList<>();
+        insCarreras.add(inscCarrera2);
+        carrera.setInscripciones(insCarreras);
+        carrera = carreraRepository.save(carrera);
+
+        // Crear asignatura
+        Asignatura asignatura = new Asignatura();
+        asignatura.setNombre("Asignatura Test 1");
+        asignatura.setCarrera(carrera);
+        asignatura = asignaturaRepository.save(asignatura);
+        List<Asignatura> asignaturas = new ArrayList<>();
+        asignaturas.add(asignatura);
+        carrera.setAsignaturas(asignaturas);
+        carrera = carreraRepository.save(carrera);
+
+        //Crear Curso
+        Curso curso = new Curso();
+        LocalDate fechaInicio = LocalDate.of(2025, 3, 15); // Año, Mes (1-12), Día
+        curso.setFechaInicio(fechaInicio);
+        LocalDate fechaFin = LocalDate.of(2025, 7, 15); // Año, Mes (1-12), Día
+        curso.setFechaFin(fechaFin);
+        curso.setAsignatura(asignatura);
+        curso.setEstado(Estado.ACTIVO);
+        curso.setDiasPrevInsc(30);
+        //curso = cursoRepository.save(curso);
+
+        //Crear Inscripcion Curso
+        InscripcionCurso inscripcionCurso = new InscripcionCurso();
+        inscripcionCurso.setFechaInscripcion(LocalDateTime.now());
+        inscripcionCurso.setEstudiante(estudiante);
+        inscripcionCurso.setCurso(curso);
+        inscripcionCurso.setCalificacion(CalificacionCurso.PENDIENTE);
+        inscripcionCurso = inscripcionCursoRepository.save(inscripcionCurso);
+
+        curso.getInscripciones().add(inscripcionCurso);
+        curso = cursoRepository.save(curso);
+
+        //Crear Inscripcion Curso
+        InscripcionCurso inscripcionCurso2 = new InscripcionCurso();
+        inscripcionCurso2.setFechaInscripcion(LocalDateTime.now());
+        inscripcionCurso2.setEstudiante(estudiante2);
+        inscripcionCurso2.setCurso(curso);
+        inscripcionCurso2.setCalificacion(CalificacionCurso.PENDIENTE);
+        inscripcionCurso2 = inscripcionCursoRepository.save(inscripcionCurso2);
+
+        curso.getInscripciones().add(inscripcionCurso2);
+        curso = cursoRepository.save(curso);
+
+        mockMvc.perform(get("/cursos/" + curso.getId() + "/estudiantes")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").value(estudiante.getId()))
+                .andExpect(jsonPath("$[0].nombre", is(estudiante.getNombre())))
+                .andExpect(jsonPath("$[0].apellido").value(estudiante.getApellido()))
+                .andExpect(jsonPath("$[0].ci").value(estudiante.getCi()));
     }
 }
