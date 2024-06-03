caffcb1767abc1fd60d62b8717e3b7356c4437c4
Gonzalo Cánepa <gcanepa28@gmail.com>
2024-06-03 15:23:42 -0300

    Obtener Estudiantes por Id de Curso.

--- src/main/java/com/tecnoinf/gestedu/services/interfaces/CursoService.java
+++ src/main/java/com/tecnoinf/gestedu/services/interfaces/CursoService.java
@@ -2,8 +2,12 @@ package com.tecnoinf.gestedu.services.interfaces;
 
 import com.tecnoinf.gestedu.dtos.curso.CursoDTO;
 import com.tecnoinf.gestedu.dtos.curso.HorarioDTO;
+import com.tecnoinf.gestedu.dtos.usuario.UsuarioDTO;
+
+import java.util.List;
 
 public interface CursoService {
     CursoDTO createCurso(CursoDTO nuevoCurso);
     HorarioDTO addHorarioToCurso(Long cursoId, HorarioDTO nuevoHorario);
+    List<UsuarioDTO> getEstudiantesByCurso(Long cursoId);
 }
