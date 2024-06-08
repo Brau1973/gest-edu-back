2cceca906a3cecf8b94839caa3ab29f86ea57e9a
Gonzalo Cánepa <gcanepa28@gmail.com>
2024-06-08 16:11:44 -0300

    Dar de baja una inscripción de un estudiante

--- src/main/java/com/tecnoinf/gestedu/dtos/inscripcionCurso/InscripcionCursoDTO.java
+++ src/main/java/com/tecnoinf/gestedu/dtos/inscripcionCurso/InscripcionCursoDTO.java
@@ -1,13 +1,31 @@
 package com.tecnoinf.gestedu.dtos.inscripcionCurso;
 
+import com.tecnoinf.gestedu.models.InscripcionCurso;
+import com.tecnoinf.gestedu.models.enums.CalificacionCurso;
+import com.tecnoinf.gestedu.models.enums.EstadoInscripcionCurso;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class InscripcionCursoDTO {
+    private Long id;
+    private LocalDateTime fechaInscripcion;
+    private EstadoInscripcionCurso estado;
+    private CalificacionCurso calificacion;
     private Long estudianteId;
     private Long cursoId;
+
+    public InscripcionCursoDTO(InscripcionCurso inscripcion) {
+        this.id = inscripcion.getId();
+        this.fechaInscripcion = inscripcion.getFechaInscripcion();
+        this.estado = inscripcion.getEstado();
+        this.calificacion = inscripcion.getCalificacion();
+        this.estudianteId = inscripcion.getEstudiante().getId();
+        this.cursoId = inscripcion.getCurso().getId();
+    }
 }
