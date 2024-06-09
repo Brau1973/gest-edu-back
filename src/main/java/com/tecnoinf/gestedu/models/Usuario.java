27996af8d9c1bc8199422f2c96c239ca6c4ea692
silBH <berrueta.hermida@gmail.com>
2024-06-09 09:59:45 -0300

    tests sin terminar

--- src/main/java/com/tecnoinf/gestedu/models/Usuario.java
+++ src/main/java/com/tecnoinf/gestedu/models/Usuario.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-public abstract class Usuario {
+public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
