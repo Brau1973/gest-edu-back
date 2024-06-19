7c77856044aec49912df888eba450794748dbebd
silBH <berrueta.hermida@gmail.com>
2024-06-19 13:44:26 -0300

    feature/buscarCalificacionesAsignatura2)

--- src/main/java/com/tecnoinf/gestedu/repositories/CursoRepository.java
+++ src/main/java/com/tecnoinf/gestedu/repositories/CursoRepository.java
@@ -1,10 +1,14 @@
 package com.tecnoinf.gestedu.repositories;
 
 import com.tecnoinf.gestedu.models.Curso;
+import com.tecnoinf.gestedu.models.enums.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, Long> {
     boolean existsByDocenteId(Long id);
+    List<Curso> findByAsignaturaIdAndEstado(Long asignaturaId, Estado estado);
 }
