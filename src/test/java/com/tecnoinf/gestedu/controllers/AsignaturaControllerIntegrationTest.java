23523f3f6c8e05eddd1ce53f017e41e9a587319e
silBH <berrueta.hermida@gmail.com>
2024-06-19 13:33:02 -0300

    arreglo correo test

--- src/test/java/com/tecnoinf/gestedu/controllers/AsignaturaControllerIntegrationTest.java
+++ src/test/java/com/tecnoinf/gestedu/controllers/AsignaturaControllerIntegrationTest.java
@@ -21,6 +21,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -41,6 +42,7 @@ import static org.hamcrest.Matchers.is;
 @SpringBootTest(properties = {
         "spring.jpa.hibernate.ddl-auto=update"
 })
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 //@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AsignaturaControllerIntegrationTest {
