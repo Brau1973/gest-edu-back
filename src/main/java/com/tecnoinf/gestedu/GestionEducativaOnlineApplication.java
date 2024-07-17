6c94b1f2ca1792bd7d8e271acbf07b2aea1b3b1a
brauliojaureguy <brauliojaureguy12@gmail.com>
2024-07-17 20:05:39 -0300

    cambio de fecha de examen init data minus days 1

--- src/main/java/com/tecnoinf/gestedu/GestionEducativaOnlineApplication.java
+++ src/main/java/com/tecnoinf/gestedu/GestionEducativaOnlineApplication.java
@@ -280,7 +280,7 @@ public class GestionEducativaOnlineApplication {
                 //------------------------------------------------------------------------------------- EXAMEN -------------------------------------------------------------------------------------
 				// ACTIVOS
 				Examen examenCOE = createAndSaveExamen(examenRepository, LocalDateTime.now().plusDays(7).truncatedTo(ChronoUnit.HOURS), 20, Estado.ACTIVO, COE, docente1);
-				Examen examenPAV = createAndSaveExamen(examenRepository, LocalDateTime.now().plusDays(0).truncatedTo(ChronoUnit.HOURS), 20, Estado.ACTIVO, PAV, docente3);
+				Examen examenPAV = createAndSaveExamen(examenRepository, LocalDateTime.now().minusDays(1).truncatedTo(ChronoUnit.HOURS), 20, Estado.ACTIVO, PAV, docente3);
 				Examen examenMDL1 = createAndSaveExamen(examenRepository, LocalDateTime.now().minusDays(3).truncatedTo(ChronoUnit.HOURS), 20, Estado.ACTIVO, MDL1, docente2);
 
 				// FINALIZADOS
